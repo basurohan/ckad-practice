ffcd176a1d8803a4f1d9352af424e9a6e202c545
Rohan Basu <basurohan@gmail.com>
2019-03-30 23:36:03 -0400

    Added nginx config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    messages: [],
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:80/api/message")
+    .then((result) => {
+      this.setState({ messages: result.data })
+    }).catch((err) => {
+      console.log(err)
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           Messages Received from Server
         </header>
+        <div>
+          { this.state.messages.map(message => <ul>{ message.message }</ul>) }
+        </div>
       </div>
     );
   }
