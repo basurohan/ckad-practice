b344268386252bc71f081b701f23597bd511b9a6
Rohan Basu <basurohan@gmail.com>
2019-04-08 15:11:56 -0400

    Updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
-import keys from './keys';
+const keys = require('./keys');
 
 class App extends Component {
 
